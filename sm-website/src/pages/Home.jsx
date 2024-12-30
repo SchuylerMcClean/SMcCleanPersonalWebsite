d77790e726f9bf4c71bc6ad293a5ff966ab3d9e7
TheSkybo <schuylermcclean@gmail.com>
2024-12-29 23:48:10 -0500

    Edited home page.

--- sm-website/src/pages/Home.jsx
+++ sm-website/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import profilePic from "/schuyprofilepic.jpg";
 import forestPic from "/forestPic.jpg";
+import resumePic from "/tempResume.png";
 
 function Home() {
   //const [count, setCount] = useState(0);
@@ -15,32 +16,35 @@ function Home() {
           <p className="home-about-text">
             I am a first year student at the University of Guelph, in the
             Honours Computer Science program. Click below to learn more about
-            me.
+            me!
           </p>
           <a href="/about" style={{ textDecoration: "underline" }}>
             Click here to learn more -&#62;
           </a>
         </div>
         <div>
-          <img src={profilePic} className="logo react" alt="React logo" />
+          <img src={profilePic} className="profile-picture" />
         </div>
       </div>
-      <div className="home-forest-box">
-        <img className="home-forest-pic" src={forestPic} />
+      <div>
+        <img src={forestPic} className="home-forest-pic" />
       </div>
-      <div className="home-white-box">
-        <h1>Experience and Resume</h1>
-        <p>
-          I have formal education in Python, Java, and C, as well as personal
-          experience in JavaScript, CSS, SQL, and Rust.
-        </p>
-        <a
-          href="/about"
-          className="home-white-box-items"
-          style={{ textDecoration: "underline", color: "#284b19" }}
-        >
-          Click here to learn more -&#62;
-        </a>
+      <div
+        style={{ justifyContent: "center", display: "flex", flexWrap: "wrap" }}
+      >
+        <div>
+          <img src={resumePic} className="profile-picture" />
+        </div>
+        <div className="home-about-box">
+          <h1>Experience and Resume</h1>
+          <p className="home-about-text">
+            I have formal education in Python, Java, and C, as well as personal
+            experience in JavaScript, CSS, SQL, and Rust.
+          </p>
+          <a href="/about" style={{ textDecoration: "underline" }}>
+            Click here to learn more -&#62;
+          </a>
+        </div>
       </div>
     </>
   );
